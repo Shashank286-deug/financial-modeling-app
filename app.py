7109316752cab18d10e51026ada374659017cadf
Shashank286-deug <mrunique060110@gmail.com>
2025-04-09 21:35:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import tempfile
 import shutil
 import pandas as pd
 
-API_KEY = "Q8LU981EWC83K7VI"
+API_KEY = "VTZPNE4PSIV8GYXM"
 
 def fetch_income_statement(symbol):
     url = f"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol={symbol}&apikey={API_KEY}"
