a63c51cd2205decb6c08a2ff05a837cec8a590fb
Shashank286-deug <mrunique060110@gmail.com>
2025-04-10 00:26:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import yfinance as yf
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-import finnhub
 import plotly.express as px
 import smtplib
 from email.message import EmailMessage
