0c32f1cebcea9e4584650dcbd29288f1da680b99
Shashank286-deug <mrunique060110@gmail.com>
2025-04-10 00:26:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import yfinance as yf
 import requests
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import finnhub
 import plotly.express as px
